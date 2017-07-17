29e7f8f1aa09b5b9fca246a254c29f36172d8e17
Julian Gruber <julian@juliangruber.com>
2017-07-17 16:02:05 +0200

    ignore query strings

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const send = require('send')
+const { parse } = require('url')
 
 module.exports = opts => (req, res) =>
   new Promise((resolve, reject) => {
-    send(req, req.url, opts)
+    send(req, parse(req.url).pathname, opts)
       .on('error', err => {
         if (err.code !== 'ENOENT') return reject(err)
         resolve(false)
