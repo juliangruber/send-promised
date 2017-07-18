bf90cbe7b5ff7bbf3e0d9848c46f1828bb324e34
Julian Gruber <julian@juliangruber.com>
2017-07-18 09:28:04 +0200

    add opts extension

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const send = require('send')
 const { parse } = require('url')
 
-module.exports = opts => (req, res) =>
+module.exports = opts => (req, res, _opts = {}) =>
   new Promise((resolve, reject) => {
-    send(req, parse(req.url).pathname, opts)
+    send(req, parse(req.url).pathname, Object.assign({}, opts, _opts))
       .on('error', err => {
         if (err.code !== 'ENOENT') return reject(err)
         resolve(false)
